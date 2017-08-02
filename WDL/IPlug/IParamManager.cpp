bca4f62a3c872818fed55c7948092acb565dd44f
csmacdonald95 <csmacdonald95@gmail.com>
2017-08-02 18:47:14 -0300

    Add IParamManager

---
+++ WDL/IPlug/IParamManager.cpp
@@ -0,0 +1,80 @@
+#include "IParamManager.h"
+
+IParamManager::IParamManager(IPlugBase* pPlug) : mPlug(pPlug)
+{
+    mHandlers = new ParamChangeHandler [pPlug->NParams()];
+    for (int i = 0; i < pPlug->NParams(); ++i) {
+        mHandlers[i] = NULL;
+        mFreeParamIdxs.Add(new int(i));
+    }
+}
+
+void IParamManager::HandleParamChange(int paramIdx)
+{
+    ParamChangeHandler handler = mHandlers[paramIdx];
+    if (handler != NULL) {
+       (mPlug->*handler)(paramIdx);
+    }
+}
+
+const int IParamManager::AddBool(ParamChangeHandler handler, const char* name, bool defaultVal, const char* label, const char* group)
+{
+    int paramIdx = GetFreeParamIdx();
+    if (paramIdx < 0) return paramIdx;
+
+    mPlug->GetParam(paramIdx)->InitBool(name, defaultVal, label, group);
+
+    SetHandler(paramIdx, handler);
+
+    return paramIdx;
+}
+
+const int IParamManager::AddEnum(ParamChangeHandler handler, const char* name, int defaultVal, int nEnums, const char* label, const char* group)
+{
+    int paramIdx = GetFreeParamIdx();
+    if (paramIdx < 0) return paramIdx;
+
+    mPlug->GetParam(paramIdx)->InitEnum(name, defaultVal, nEnums, label, group);
+
+    SetHandler(paramIdx, handler);
+
+    return paramIdx;
+}
+
+const int IParamManager::AddInt(ParamChangeHandler handler, const char* name, int defaultVal, int minVal, int maxVal, const char* label, const char* group)
+{
+    int paramIdx = GetFreeParamIdx();
+    if (paramIdx < 0) return paramIdx;
+
+    mPlug->GetParam(paramIdx)->InitInt(name, defaultVal, minVal, maxVal, label, group);
+
+    SetHandler(paramIdx, handler);
+
+    return paramIdx;
+}
+
+const int IParamManager::AddDouble(ParamChangeHandler handler, const char* name, double defaultVal, double minVal, double maxVal, double step, const char* label, const char* group, double shape)
+{
+    int paramIdx = GetFreeParamIdx();
+    if (paramIdx < 0) return paramIdx;
+
+    mPlug->GetParam(paramIdx)->InitDouble(name, defaultVal, minVal, maxVal, step, label, group, shape);
+
+    SetHandler(paramIdx, handler);
+
+    return paramIdx;
+}
+
+const int IParamManager::GetFreeParamIdx()
+{
+    if (mFreeParamIdxs.GetSize()) 
+    {
+        // Get next free param idx
+        int paramIdx = *mFreeParamIdxs.Get(0);
+        // Remove param idx from list
+        mFreeParamIdxs.Delete(0);
+
+        return paramIdx;
+    } 
+    else return -1; // No free params
+}
---
+++ WDL/IPlug/IParamManager.h
@@ -0,0 +1,42 @@
+#ifndef _IPARAMMANAGER_
+#define _IPARAMMANAGER_
+
+#include "IPlugBase.h"
+
+// IParamManager is a simple way of automatically delgating param idxs and
+// registering handlers for param changes. This class must be used entirely for
+// param idxs as this class doesn't directly communicate with the plug
+
+// void IPlug member functions that take a param idx as an argument
+typedef void (IPlugBase::*ParamChangeHandler) (int);
+
+class IParamManager
+{
+public:
+    IParamManager(IPlugBase* pPlug);
+    ~IParamManager() {}
+
+    // Should be called by OnParamChange
+    void HandleParamChange(int paramIdx);
+
+    // Defines a new parameter on a free param Idx; returns -1 if there are no free params left
+    const int AddBool(ParamChangeHandler handler, const char* name, bool defaultVal, const char* label = "", const char* group = "");
+    const int AddEnum(ParamChangeHandler handler, const char* name, int defaultVal, int nEnums, const char* label = "", const char* group = "");
+    const int AddInt(ParamChangeHandler handler, const char* name, int defaultVal, int minVal, int maxVal, const char* label = "", const char* group = "");
+    const int AddDouble(ParamChangeHandler handler, const char* name, double defaultVal, double minVal, double maxVal, double step, const char* label = "", const char* group = "", double shape = 1.);
+
+    const int GetFreeParamIdx (void);
+
+    inline void SetHandler(int paramIdx, ParamChangeHandler handler) { mHandlers[paramIdx] = handler; }
+    inline ParamChangeHandler GetHandler(int paramIdx) { return mHandlers[paramIdx]; }
+
+    inline IPlugBase* GetPlug() { return mPlug; }
+
+private:
+    ParamChangeHandler* mHandlers;
+    WDL_PtrList<int> mFreeParamIdxs;
+
+    IPlugBase* mPlug;
+};
+
+#endif 
